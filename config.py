6cbaf4a951f865228b8334b5e1a862543303b8ca
jophita-kristen-s <jophitakristens@gmail.com>
2026-06-30 19:34:28 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
+
+
 class Config:
 
     SECRET_KEY = "secret-key"
 
-    SQLALCHEMY_DATABASE_URI = (
+    """SQLALCHEMY_DATABASE_URI = (
         "postgresql://postgres:password@localhost/chatdb"
-    )
+    )"""
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
